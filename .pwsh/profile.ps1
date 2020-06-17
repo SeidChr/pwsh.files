d055917e3667484836c51a269b839a6731709855
Christian Seidlitz <seidchr+github@gmail.com>
2020-06-17 16:35:05 +0200

    update profile to match new files

--- .pwsh/profile.ps1
+++ .pwsh/profile.ps1
@@ -1,28 +1,7 @@
+. $PSScriptRoot/functions.ps1
+. $PSScriptRoot/prompt.ps1
+
 Write-Host PWSH $($psversiontable.PSEdition) $($psversiontable.PSVersion)
-
-function prompt {
-    $homePath = Resolve-Path ~
-    $locationPath = Get-Location;
-    $leaf = Split-Path -Leaf $locationPath
-    $parent = Split-Path -Parent $locationPath
-
-    #(Get-Date -UFormat '%y/%m/%d %R').Tostring()
-    # wont change vscode name
-    $Host.UI.RawUI.WindowTitle = $leaf
-
-    #$parent = $direcotry.Parent;
-    #$parentFull = $parent.FullName
-    $parentShort = $parent.Replace($homePath, "~")
-
-    Write-Host
-    Write-Host "> " -NoNewline -ForegroundColor Red
-    Write-Host $parentShort -NoNewline -ForegroundColor Blue
-    Write-Host "$([System.IO.Path]::DirectorySeparatorChar)" -NoNewline -ForegroundColor Red
-    Write-Host ($leaf + " ") -ForegroundColor Green
-
-    Write-Host "$" -NoNewline -ForegroundColor DarkGray
-    return     " "
-}
 
 # allow calling scripts without '.\' prefix
 $env:path = $env:path + ";."
