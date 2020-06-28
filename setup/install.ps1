2af6c05bb05e041f375749cfa197f8473b01cf64
Christian Seidlitz <seidchr+github@gmail.com>
2020-06-28 19:29:45 +0200

    proper file move

--- setup/install.ps1
+++ setup/install.ps1
@@ -1,16 +1,25 @@
 $account = "SeidChr"
 $repository = "pwsh.files"
+$destination = "~/.pwsh"
+$version = "master"
 
-$url = "https://github.com/$account/$repository/archive/master.zip"
-$download_path = "$env:TEMP\dotfiles\master.zip"
+$url = "https://github.com/$account/$repository/archive/$version.zip"
+$downloadPath = "$env:TEMP\dotfiles\$version.zip"
+$unzipPath = "$env:TEMP\dotfiles\$version"
 
-New-Item -ItemType Directory -Force -Path (Split-Path $download_path -Parent)
-Invoke-WebRequest -Uri $url -OutFile $download_path
+New-Item -ItemType Directory -Force -Path (Split-Path $downloadPath -Parent)
+Invoke-WebRequest -Uri $url -OutFile $downloadPath
 
-Get-Item $download_path | Unblock-File
+Get-Item $downloadPath | Unblock-File
 
-Expand-Archive -Path $download_path -DestinationPath ~/.pwsh -Force
+Expand-Archive -Path $downloadPath -DestinationPath $unzipPath -Force
 
-Remove-Item $download_path
+#$extractedFilesFilter = "$unzipPath/$repository-$version/**/*.*"
+#Get-ChildItem -Path $extractedFilesFilter -Recurse | Move-Item -Destination $destination -WhatIf
+$extractedFilesFilter = "$unzipPath/$repository-$version/*"
+Copy-Item $extractedFilesFilter -Destination $destination -Recurse -Force
+#Move-Item -Path $extractedFilesFilter -Destination $destination -Force
+
+#Remove-Item $downloadPath
 
 # iwr -Uri 'https://raw.githubusercontent.com/SeidChr/pwsh.files/master/setup/install.ps1' | iex
