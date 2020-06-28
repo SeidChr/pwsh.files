fdf44213b5f2332e5d6108b1a4cbc9c8b64c708a
Christian Seidlitz <seidchr+github@gmail.com>
2020-06-28 20:46:49 +0200

    use path separator to be system independent

--- profile.ps1
+++ profile.ps1
@@ -4,7 +4,7 @@
 Write-Host PWSH $($psversiontable.PSEdition) $($psversiontable.PSVersion)
 
 # allow calling scripts without '.\' prefix
-$env:path = $env:path + ";."
+$env:path = $env:path + [System.IO.Path]::PathSeparator + "."
 
 # Shows navigable menu of all options when hitting Tab
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
