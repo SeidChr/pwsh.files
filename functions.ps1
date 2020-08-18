23200e0bb06566a5b9892663a3db2b61dbf81b8b
Christian Seidlitz <seidchr+github@gmail.com>
2020-08-18 18:54:58 +0200

    add start-parallel command

--- functions.ps1
+++ functions.ps1
@@ -67,3 +67,33 @@ function Get-LastWriteTime {
     Get-ChildItem -Directory $path -Recurse -Filter $filter | % { $_.LastWriteTimeUtc } | Sort-Object -Descending -Top 1
     #Get-ChildItem $path -Recurse -Filter $filter | % { $_.LastWriteTimeUtc } | Measure -Maximum
 }
+
+function Start-Parallel {
+    param([ScriptBlock[]] [Parameter(Position=0)]$ScriptBlock)
+
+    $jobs = $ScriptBlock | ForEach-Object { Start-Job -ScriptBlock $_ }
+    $colors = "Blue", "Red", "Cyan", "Green", "Magenta"
+    $colorCount = $colors.Length
+
+    try {
+        while (($jobs | Where-Object { $_.State -ieq "running" } | Measure-Object).Count -gt 0) {
+            $jobs | ForEach-Object { $i = 1 } {
+                $fgColor = $colors[($i - 1) % $colorCount]
+                $out = $_ | Receive-Job
+                $out = $out -split [System.Environment]::NewLine
+                $out | ForEach-Object {
+                    Write-Host "$i> "-NoNewline -ForegroundColor $fgColor
+                    Write-Host $_
+                }
+                
+                $i++
+            }
+        }
+    }
+    finally {
+        Write-Host "Stopping Parallel Jobs ..." -NoNewline
+        $jobs | Stop-Job
+        $jobs | Remove-Job -Force
+        Write-Host " done."
+    }
+}
