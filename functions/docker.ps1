6451925634ba7de231edb0ac642d05671c0296f4
Christian Seidlitz <seidchr+github@gmail.com>
2020-09-18 13:03:16 +0200

    adding docker 4 win autostart and mapped folders

--- functions/docker.ps1
+++ functions/docker.ps1
@@ -1,17 +1,43 @@
 function Get-DockerShell {
     param(
         $image = "debian",
+        [Alias("shell")]
         $entrypoint,
-        $shell #alias for entrypoint
+        [Alias("mappedFolderPath")]
+        $mapFrom,
+        $mapTo = "project",
+        [switch]
+        $start = $false
     )
 
-    if (![string]::IsNullOrWhiteSpace($shell)) { 
-        $entrypoint = $shell
+    if ($start -and $IsWindows) {
+        $checkDocker4WinStopped = { -not (Get-Process 'com.docker.proxy' -ea SilentlyContinue) }
+        if (& $checkDocker4WinStopped) {
+            Write-Host "Starting Docker 4 Windows..." -NoNewline
+            Start-Process (Join-Path $env:ProgramFiles "Docker\Docker\Docker Desktop.exe")
+            while (& $checkDocker4WinStopped) {
+                Start-Sleep -Seconds 20
+            }
+
+            Write-Host "done."
+        } else {
+            Write-Host "Docker 4 Windows is already running."
+        }
     }
 
-    if (![string]::IsNullOrWhiteSpace($entrypoint)) {
-        & docker run -it --rm --entrypoint $entrypoint $image
-    } else {
-        & docker run -it --rm $image
+    $entrypointArgument = "";
+    $mappingArgument = ""
+
+    if ($entrypoint) {
+        $entrypointArgument = "--entrypoint $entrypoint";
     }
+
+    if ($mapFrom) {
+        $mappingArgument = "-v `"$(Resolve-Path $mapFrom):/$mapTo`""
+    }
+
+    $cmd = "docker run -it --rm $mappingArgument $entrypointArgument $image";
+    Write-Host "Command: " $cmd
+
+    Invoke-Expression $cmd
 }
