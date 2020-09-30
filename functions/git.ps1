a3362e81fd1103c1f6a85a14f20333f892235d25
Christian Seidlitz <seidchr+github@gmail.com>
2020-09-30 20:39:28 +0200

    backing exit code for git command

--- functions/git.ps1
+++ functions/git.ps1
@@ -71,7 +71,11 @@ function Test-GitClean {
     param($Path = (Get-Location))
     $command = "git -C `"$Path`" status -z"
     # Write-Host $command
+
+    $backupExitCode = $global:LASTEXITCODE
     $statusString = Invoke-Expression $command | Out-String
+    $global:LASTEXITCODE = $backupExitCode
+
     !($statusString)
 }
 
