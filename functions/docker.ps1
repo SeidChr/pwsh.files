f1ecbec02c34a96b49c464a428d9a5d9fb7b4f75
Christian Seidlitz <seidchr+github@gmail.com>
2020-11-29 22:32:53 +0100

    fix docker-test

--- functions/docker.ps1
+++ functions/docker.ps1
@@ -1,5 +1,10 @@
 function Test-DockerAvailable {
-    $null = & docker version *>&1
+    $res = & docker version *>&1 | Out-String
+    if ($res -like "*error during connect*") 
+    {
+        return $false 
+    }
+
     $LASTEXITCODE -eq 0
 }
 
