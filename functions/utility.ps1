0a7d4283d017329a0cd2dea8a3ac3240929e683d
Christian Seidlitz <seidchr+github@gmail.com>
2021-09-28 12:40:35 +0200

    performance utility

--- functions/utility.ps1
+++ functions/utility.ps1
@@ -538,3 +538,97 @@ function _ {
         $pipe.End()
     }
 }
+
+# wtf powershell... why dont you have this?
+function Join-String {
+    begin {
+        $sb = [System.Text.StringBuilder]::new() 
+    } 
+    process {
+        $sb.Append($_) | Out-Null 
+    } 
+    end {
+        $sb.ToString() 
+    } 
+}
+
+# https://powershell.one/tricks/performance/pipeline
+function Where-ObjectFast {
+    param
+    (
+        [ScriptBlock]
+        $FilterScript
+    )
+  
+    begin {
+        # construct a hard-coded anonymous simple function:
+        $code = @"
+& {
+  process { 
+    if ($FilterScript) 
+    { `$_ }
+  }
+}
+"@
+        # turn code into a scriptblock and invoke it
+        # via a steppable pipeline so we can feed in data
+        # as it comes in via the pipeline:
+        $pip = [ScriptBlock]::Create($code).GetSteppablePipeline()
+        $pip.Begin($true)
+    }
+    process {
+        # forward incoming pipeline data to the custom scriptblock:
+        $pip.Process($_)
+    }
+    end {
+        $pip.End()
+    }
+}
+
+# https://powershell.one/tricks/performance/pipeline
+function Foreach-ObjectFast {
+    param
+    (
+        [ScriptBlock]
+        $Process,
+    
+        [ScriptBlock]
+        $Begin,
+    
+        [ScriptBlock]
+        $End
+    )
+  
+    begin {
+        # construct a hard-coded anonymous simple function from
+        # the submitted scriptblocks:
+        $code = @"
+& {
+  begin
+  {
+    $Begin
+  }
+  process
+  {
+    $Process
+  }
+  end
+  {
+    $End
+  }
+}
+"@
+        # turn code into a scriptblock and invoke it
+        # via a steppable pipeline so we can feed in data
+        # as it comes in via the pipeline:
+        $pip = [ScriptBlock]::Create($code).GetSteppablePipeline()
+        $pip.Begin($true)
+    }
+    process {
+        # forward incoming pipeline data to the custom scriptblock:
+        $pip.Process($_)
+    }
+    end {
+        $pip.End()
+    }
+}
