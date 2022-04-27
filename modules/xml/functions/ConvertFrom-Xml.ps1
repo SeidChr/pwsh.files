7b9d78b76c716799e69a95ad18161922b5b30531
Christian Seidlitz <christian.seidlitz@thinfabrics.com>
2022-04-27 12:39:39 +0200

    fix non-cleared xml writer by recreating objects

--- modules/xml/functions/ConvertFrom-Xml.ps1
+++ modules/xml/functions/ConvertFrom-Xml.ps1
@@ -3,16 +3,12 @@ param(
     [xml] $InputObject
 )
 
-begin {
+process {
     $stringWriter = New-Object System.IO.StringWriter
     $xmlWriter = New-Object System.XMl.XmlTextWriter $stringWriter
     $xmlWriter.Formatting = "indented"
     $xmlWriter.Indentation = 4
 
-    $stringBuilder = $stringWriter.GetStringBuilder()
-}
-
-process {
     $InputObject.WriteContentTo($XmlWriter)
     $xmlWriter.Flush()
     $stringWriter.Flush()
@@ -20,8 +16,8 @@ process {
     # return result
     $stringWriter.ToString()
     
-    # clear string writer
-    $null = $stringBuilder.Clear()
+    # clear stream variables
+    Remove-Variable "stringWriter", "xmlWriter" -Scope Local
 }
 
 # test: "<test name=`"testname`"><testing/><start>some</start><end>test</end><other/><evenMore/></test>","<test2>bla</test2>" |% {[xml]$_} | ConvertFrom-Xml
