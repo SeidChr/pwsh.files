f996b5ccca765e247d71e0fc044dba654281d89b
Christian Seidlitz <christian.seidlitz@thinfabrics.com>
2023-01-05 14:10:13 +0100

    fix casing

--- modules/docker/functions/Get-DockerShell.ps1
+++ modules/docker/functions/Get-DockerShell.ps1
@@ -1,20 +1,23 @@
 param(
     [Parameter(Position = 0)]
-    $image = "debian",
+    $Image = "debian",
 
     [Parameter(Position = 1)]
     [Alias("shell")]
-    $entrypoint,
+    $Entrypoint,
 
     [Alias("mappedFolderPath")]
-    $mapFrom,
+    $MapFrom,
 
-    $mapTo = "project",
+    $MapTo = "project",
 
     [Alias("w")]
     [Alias("wd")]
     [Alias("location")]
-    $workingDirectory
+    $WorkingDirectory,
+
+    [Alias("u")]
+    $User
 )
 
 Start-Docker -nomsg
@@ -23,40 +26,44 @@ $entrypointArgument = ""
 $mappingArgument = ""
 $workingDirectoryArgument = ""
 
-$image = switch ($image) {
+$Image = switch ($Image) {
     ".netsdk" { "mcr.microsoft.com/dotnet/sdk"; break }
     ".netasp" { "mcr.microsoft.com/dotnet/core/aspnet"; break }
     { $_ -in ".net", ".netrt" } { "mcr.microsoft.com/dotnet/runtime"; break }
     { $_ -in ".netdeps", ".netrtdeps" } { "mcr.microsoft.com/dotnet/runtime-deps"; break }
     { $_ -in ".ps", ".pwsh", ".powershell" } { "mcr.microsoft.com/powershell"; break }
-    default { $image }
+    default { $Image }
 }
 
-if ($entrypoint) {
-    $entrypointArgument = "--entrypoint $entrypoint";
+if ($Entrypoint) {
+    $entrypointArgument = "--entrypoint $Entrypoint";
 }
 
-if ($mapTo -and -not ($mapTo.StartsWith("/"))) {
-    $mapTo = "/" + $mapTo;
+if ($MapTo -and -not ($MapTo.StartsWith("/"))) {
+    $MapTo = "/" + $MapTo;
 }
 
-if ($workingDirectory -and -not ($workingDirectory.StartsWith("/"))) {
-    $workingDirectory = "/" + $workingDirectory;
+if ($WorkingDirectory -and -not ($WorkingDirectory.StartsWith("/"))) {
+    $WorkingDirectory = "/" + $WorkingDirectory;
 }
 
-if ($mapFrom) {
-    if (!$workingDirectory -and $mapTo) {
-        $workingDirectory = $mapTo
+if ($MapFrom) {
+    if (!$WorkingDirectory -and $MapTo) {
+        $WorkingDirectory = $MapTo
     }
 
-    $mappingArgument = "-v `"$(Resolve-Path $mapFrom):$mapTo`""
+    $mappingArgument = "-v `"$(Resolve-Path $MapFrom):$MapTo`""
 }
 
-if ($workingDirectory) {
-    $workingDirectoryArgument = "-w `"$workingDirectory`""
+if ($WorkingDirectory) {
+    $workingDirectoryArgument = "-w `"$WorkingDirectory`""
 }
 
-$cmd = "docker run -it --rm $mappingArgument $workingDirectoryArgument $entrypointArgument $image";
+if ($User) {
+    $userArgument = "--user `"$User`""
+}
+
+$cmd = "docker run -it --rm $mappingArgument $workingDirectoryArgument $entrypointArgument $userArgument $Image";
 Write-Host "Command: " $cmd
 
 Invoke-Expression $cmd
