2ab7e2c4f7b091b9cf2886e50c5813e43e633600
Christian Seidlitz <seidchr+github@gmail.com>
2023-12-20 14:05:23 +0100

    Manage delays for running every second
    Add functionality to manage delays in the `Test-PublicIp` function so
    that it runs every second. This change ensures more accurate time
    intervals between each execution. The `DelaySeconds` parameter allows
    the user to specify the desired delay between each execution. By
    default, the delay is set to 1 second. This improvement enhances the
    precision of monitoring the public IP address and latency. These changes
    were made based on the need to have a more frequent and precise
    execution of the `Test-PublicIp` function, facilitating accurate
    tracking of network status. No issues are associated with this commit.

--- modules/util/functions/Test-PublicIp.ps1
+++ modules/util/functions/Test-PublicIp.ps1
@@ -1,21 +1,51 @@
 param(
-    [switch] $Repeat
+    [switch] $Repeat,
+    [switch] $PassThrou,
+    [int] $DelaySeconds = 1
 )
 
-function execute {
-    $ping = Test-Connection google.de -Ping -Count 1
-    $ip = Invoke-RestMethod "https://api64.ipify.org"
-    Write-Host "Time=$(Get-Date -Format "HH:mm:ss.fffffff") PublicIp=$ip Latency=$($ping.Latency)"
+$stdArgs = @{
+    ea = 'SilentlyContinue'
 }
 
-function wait {
-    Start-Sleep -Seconds 1
+function execute {
+    $result = @{
+        Time = Get-Date
+        Latency = (Test-Connection google.de -Ping -Count 1 @stdArgs).Latency
+        PublicIp = Invoke-RestMethod "https://api64.ipify.org" @stdArgs
+    }
+
+    if ($PassThrou) {
+        $result
+    } else {
+        Write-Host ("Time={0:HH:mm:ss.fff} PublicIp={1} Latency={2}" -f $result.Time, $result.PublicIp, $result.Latency)
+    }
+    # start-sleep -Milliseconds 1200
 }
+
+function waitTill {
+    param([DateTime] $Time)
+    $diffMs = -(([datetime]::Now - $Time).TotalMilliseconds)
+    if ($diffMs -gt 0) {
+        Start-Sleep -Milliseconds $diffMs
+    }
+    # if the delay is too high, add overflow
+    if ($diffMs -lt -1000) {
+        $added = [int]((-$diffMs)/1000)
+        # Write-Host ('.'*$added)
+        $Time.AddSeconds($added)
+    } else {
+        $Time
+    }
+}
+
+$nextRun = [datetime]::Now
 
 if ($Repeat) {
     while ($true) {
+        $nextRun = $nextRun.AddSeconds($DelaySeconds)
         execute
-        wait
+        $nextRun = waitTill $nextRun
     }
 } else {
     execute
