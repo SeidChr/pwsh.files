8d3fef3b139dbb6b4264d39e38fdf43119868cb0
Christian Seidlitz <seidchr+github@gmail.com>
2026-08-11 16:47:17 +0200

    add module load debugging

--- modules.ps1
+++ modules.ps1
@@ -1,3 +1,20 @@
-Get-ChildItem (Join-Path $PSScriptRoot modules) | ForEach-Object {
-    Import-Module -Force -DisableNameChecking -Name $_ # -Verbose
+$stopwatch = [System.Diagnostics.Stopwatch]::new()
+
+try {
+    Get-ChildItem (Join-Path $PSScriptRoot modules) | ForEach-Object {
+        $module = $_
+        $stopwatch.Restart()
+
+        try {
+            Import-Module -Force -DisableNameChecking -Name $module # -Verbose
+        } finally {
+            $stopwatch.Stop()
+        }
+
+        if ($stopwatch.Elapsed.TotalSeconds -gt 1) {
+            Write-Warning ("Module '{0}' took {1:N2} seconds to load." -f $module.Name, $stopwatch.Elapsed.TotalSeconds)
+        }
+    }
+} finally {
+    $stopwatch = $null
 }
